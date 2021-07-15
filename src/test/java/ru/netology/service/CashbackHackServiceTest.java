2db72122cd30ec599388f986314eaad879eccc6d
Ekaterina Kolesnikova <1005885@mail.ru>
2021-07-15 15:30:30 +0300

    Added junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,37 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturn120IfAmount880() {
+
+        int actual = service.remain(880);
+        int expected = 120;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn900IfAmount100() {
+
+        int actual = service.remain(100);
+        int expected = 900;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnZeroIfAmount1000() {
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+}
